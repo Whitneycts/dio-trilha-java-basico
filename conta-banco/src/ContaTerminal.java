01d7c75dc9be52e7a8f8c9bec5d1486f514a6ff3
Whitney Costa <whitneycosta@proton.me>
2025-08-25 17:48:01 -0300

    finalizar ContaTerminal com leitura de dados e mensagem final

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+     //Exibir as mensagens para o nosso usuário
+    System.out.println("Digite o número da conta: ");
+    int numeroConta = scanner.nextInt();
+
+    scanner.nextLine();
+
+    System.out.println("Digite o número da agência: ");
+    String agencia = scanner.nextLine();
+
+    System.out.println("Digite o nome do cliente: ");
+    String nomeCliente = scanner.nextLine();
+
+    System.out.println("Digite o saldo inicial: ");
+    double saldo = scanner.nextDouble();
+
+    String mensagem = "Olá " + nomeCliente 
+                        + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+                        + agencia + ", conta " 
+                        + numeroConta + " e seu saldo " 
+                        + saldo + " já está disponível para saque.";
+
+    System.out.println(mensagem);
+    scanner.close();
+
     }
 }
